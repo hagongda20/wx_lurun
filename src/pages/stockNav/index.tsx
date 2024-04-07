e8fc57ce96b003accd8634382738cf4da8830100
System Administrator <root@zhouyudeMacBook-Pro.local>
2024-04-07 19:43:45 +0800

    the second

--- src/pages/stockNav/index.tsx
+++ src/pages/stockNav/index.tsx
@@ -37,7 +37,7 @@ const Home = () => {
         <View className='divider' />
         <AtButton className='btn' disabled onClick={handleProductClick}>一账双录</AtButton>
         <AtButton className='btn' disabled onClick={handleProductClick}>对账审核</AtButton>
-
+        <AtButton className='btn' disabled onClick={handleProductClick}>对账审核</AtButton>
       </View>
     </View>
   );
